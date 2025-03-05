0f2d42ae1c42496c2ee9ba57730dffe4a1ee71d7
Aletziz <galexito856@gmail.com>
2025-03-05 10:06:05 -0500

    Delete components directory

--- components/WordDisplay.tsx
+++
@@ -1,7 +0,0 @@
-interface WordDisplayProps {
-  word: string;
-}
-
-export function WordDisplay({ word }: WordDisplayProps) {
-  return <div className="text-4xl font-bold text-indigo-600 mb-4">{word}</div>;
-}
